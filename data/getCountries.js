ac8932eb912b8f0bca4eeb6e1790f2571c2a40d3
Daniel <daniel.payne@keldan.co.uk>
2021-01-11 11:33:33 +0000

    updated for sql formatting

--- data/getCountries.js
+++ data/getCountries.js
@@ -1,7 +1,15 @@
 import pool from 'data/helpers/pool.js'
 
-const sql = `SELECT country_id,country_name,iso2_code,iso3_code FROM info.countries c LIMIT 10;`
+const query = `
+  SELECT 
+    country_id,
+    country_name,
+    iso2_code,
+    iso3_code 
+  FROM 
+    info.countries
+`
 
 export default function getCountries() {
-  return pool.query(sql).then((res) => res.rows)
+  return pool.query(query).then((res) => res.rows)
 }
